0317d6054627f2d7620bc9485670b16d6f5c768b
vmorozov893 <vmorozov893@gmail.com>
2024-08-10 14:31:55 +0300

    Готовое решение.

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,16 @@ public class Main {
         String lastName = "Ivanov";
         String fullName = lastName + ' ' + firstName + ' ' + middleName;
         System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("\n2 Задание.\n");
+
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName);
+
+        System.out.println("\n3 Задание.\n");
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
