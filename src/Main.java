24de45290689c3ed208390757e5793e5195e06d4
vmorozov893 <vmorozov893@gmail.com>
2024-08-10 14:14:44 +0300

    1 Задание.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("1 Задание.\n");
 
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
     }
 }
